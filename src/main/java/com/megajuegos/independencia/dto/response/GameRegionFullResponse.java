258ed7c7c38cd82f7baa5b0edae3089839c82de8
Pablo Leyes <64818001+Yaguaret3@users.noreply.github.com>
2024-05-09 14:02:52 -0300

    Control UI: Responsive Regiones

--- src/main/java/com/megajuegos/independencia/dto/response/GameRegionFullResponse.java
+++ src/main/java/com/megajuegos/independencia/dto/response/GameRegionFullResponse.java
@@ -16,6 +16,18 @@ public class GameRegionFullResponse {
     private List<GameSubRegionFullResponse> subregions;
     private List<ActionResponse> accionesMilitares;
 
+
+    private String area;
+    private String bgTop;
+    private String bgLeft;
+    private String bgScale;
+    private String fgTop;
+    private String fgLeft;
+    private String fgViewBox;
+    private String fgHeight;
+    private String fgWidth;
+    private String color;
+
     public static GameRegionFullResponse toFullResponse(GameRegion entity){
         return GameRegionFullResponse.builder()
                 .id(entity.getId())
@@ -29,6 +41,18 @@ public class GameRegionFullResponse {
                         .map(ActionResponse::toDto)
                         .collect(Collectors.toList())
                 )
+
+                .area(entity.getRegionEnum().getFgArea())
+                .bgTop(entity.getRegionEnum().getBgTop())
+                .bgLeft(entity.getRegionEnum().getBgLeft())
+                .bgScale(entity.getRegionEnum().getBgScale())
+                .fgTop(entity.getRegionEnum().getFgTop())
+                .fgLeft(entity.getRegionEnum().getFgLeft())
+                .fgViewBox(entity.getRegionEnum().getFgViewBox())
+                .fgHeight(entity.getRegionEnum().getFgHeight())
+                .fgWidth(entity.getRegionEnum().getFgWidth())
+                .color(entity.getRegionEnum().getColor())
+
                 .build();
     }
 }
