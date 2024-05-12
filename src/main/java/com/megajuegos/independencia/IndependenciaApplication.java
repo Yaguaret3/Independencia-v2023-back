f6881f9cfa69f1d50233d03e08048401f8386323
Pablo Leyes <64818001+Yaguaret3@users.noreply.github.com>
2024-05-12 00:57:48 -0300

    Auth & Settings: Create admin user when launching app

--- src/main/java/com/megajuegos/independencia/IndependenciaApplication.java
+++ src/main/java/com/megajuegos/independencia/IndependenciaApplication.java
@@ -1,12 +1,49 @@
 package com.megajuegos.independencia;
 
+import com.megajuegos.independencia.entities.UserIndependencia;
+import com.megajuegos.independencia.enums.RoleEnum;
+import com.megajuegos.independencia.repository.UserIndependenciaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class IndependenciaApplication {
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-	public static void main(String[] args) {
-		SpringApplication.run(IndependenciaApplication.class, args);
-	}
+@SpringBootApplication
+@RequiredArgsConstructor
+public class IndependenciaApplication implements CommandLineRunner {
+
+    private final UserIndependenciaRepository userRepository;
+
+    @Value("${ar.com.independencia.admin.email}")
+    private String email;
+    @Value("${ar.com.independencia.admin.username}")
+    private String username;
+    @Value("${ar.com.independencia.admin.password}")
+    private String password;
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndependenciaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+
+        Optional<UserIndependencia> admin = userRepository.findByEmail(email);
+        if (admin.isPresent()) {
+            return;
+        }
+        userRepository.save(
+                UserIndependencia.builder()
+                        .email(email)
+                        .username(username)
+                        .password(password)
+                        .roles(Stream.of(RoleEnum.ADMIN).collect(Collectors.toList()))
+                        .playerDataList(Stream.of(RoleEnum.CONTROL.createPlayerData()).collect(Collectors.toList()))
+                        .build());
+    }
 }
