e15a6326ceb4e53a734faee2714e78aeb352c97b
Pablo Leyes <64818001+Yaguaret3@users.noreply.github.com>
2024-05-14 23:55:21 -0300

    ResourceHandler

---
+++ src/main/java/com/megajuegos/independencia/config/resources/WebConfig.java
@@ -0,0 +1,15 @@
+package com.megajuegos.independencia.config.resources;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/src/assets").addResourceLocations("classpath:/static/assets");
+    }
+}
