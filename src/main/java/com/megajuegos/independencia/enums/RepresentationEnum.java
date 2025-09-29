61b3d288e9c41bd3df61660e35a336799324822d
Pablo Leyes <n.leyes.n@gmail.com>
2025-09-28 22:27:31 -0300

    Revolucionario: Population Update

--- src/main/java/com/megajuegos/independencia/enums/RepresentationEnum.java
+++ src/main/java/com/megajuegos/independencia/enums/RepresentationEnum.java
@@ -7,29 +7,29 @@ import javax.management.InstanceNotFoundException;
 @Getter
 public enum RepresentationEnum {
 
-    BUENOS_AIRES(1, "Buenos Aires", 0),
-    MONTEVIDEO(2, "Montevideo", 0),
+    BUENOS_AIRES(1, "Buenos Aires", 40),
+    MONTEVIDEO(2, "Montevideo", 15),
     CONCEPCION_DEL_URUGUAY(3, "Concepción del Uruguay", 0),
     CORRIENTES(4, "Corrientes", 0),
-    ASUNCION(5, "Asunción",0),
+    ASUNCION(5, "Asunción",15),
     SANTA_FE(6, "Santa Fe", 0),
-    CORDOBA(7, "Córdoba", 0),
+    CORDOBA(7, "Córdoba", 25),
     SAN_LUIS(8, "San Luis", 0),
-    MENDOZA(9, "Mendoza", 0),
+    MENDOZA(9, "Mendoza", 15),
     SAN_JUAN(10, "San Juan", 0),
     LA_RIOJA(11, "La Rioja", 0),
     CATAMARCA(12, "Catamarca", 0),
     SANTIAGO_DEL_ESTERO(13, "Santiago del Estero", 0),
-    TUCUMAN(14, "Tucumán", 0),
-    SALTA(15, "Salta", 0),
+    TUCUMAN(14, "Tucumán", 15),
+    SALTA(15, "Salta", 20),
     JUJUY(16, "Jujuy", 0),
     TARIJA(17, "Tarija", 0),
-    POTOSI(18, "Potosí", 0),
+    POTOSI(18, "Potosí", 40),
     CHUQUISACA(19, "Chuquisaca", 0),
     COCHABAMBA(20, "Cochabamba", 0),
-    LA_PAZ(21, "La Paz", 0),
-    RIO_CUARTO(22, "Río Cuarto", 6),
-    MISIONES(23, "Misiones", 14);
+    LA_PAZ(21, "La Paz", 40),
+    RIO_CUARTO(22, "Río Cuarto", 0),
+    MISIONES(23, "Misiones", 0);
 
     private Integer id;
     private String nombre;
